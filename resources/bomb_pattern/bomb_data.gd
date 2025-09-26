d3d26db06b8da9b8a7fcc6f60c76a456fa0465f8
_michdev <gamedev.mich@gmail.com>
2025-09-26 12:26:10 -0400

    added bomb pattern data type

---
+++ resources/bomb_pattern/bomb_data.gd
@@ -0,0 +1,6 @@
+class_name BombData extends Resource
+
+@export var scene: PackedScene
+@export var delay_sec: float = 0.
+@export_flags("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+var possible_positions: int = 1023
---
+++ resources/bomb_pattern/bomb_pattern.gd
@@ -0,0 +1,5 @@
+class_name BombPattern extends Resource
+
+@export var data: Array[BombData]
+@export var cycles_before_spawnable: int = 0
+@export var minimum_time: float = 0.
